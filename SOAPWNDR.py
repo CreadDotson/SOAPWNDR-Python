3192180b0773d488ddaee15f951b07676a5372a5
cread <creaddotson@gmai.com>
2017-11-22 14:25:28 -0600

    added command line arguments

--- SOAPWNDR.py
+++ SOAPWNDR.py
@@ -2,6 +2,7 @@
 
 import requests
 from BeautifulSoup import  BeautifulSoup
+import sys
 
 addr = "192.168.1.1"
 port = 80
@@ -44,14 +45,19 @@ actions = [
     }
 ]
 
-temp_actions = [
-    {
-        'action': 'urn:NETGEAR-ROUTER:service:DeviceInfo:1#GetInfo',
-        'name': 'Extracting device info...',
-        'process': get_device_info
-    }
-]
+def process_args(args):
+    global addr
+    global port
+    global ssl
+    for arg in args:
+        if arg[0:10] == '--address=':
+            addr = arg[10:]
+        if arg[0:7] == '--port=':
+            port = int(arg[7:])
+        if arg[0:6] == '--ssl=':
+            ssl = bool(arg[6:])
 
+process_args(sys.argv)
 
 endpoint = ("https://" if ssl else "http://") + addr + ':' + str(port)
 print '[!] Attempting to extract information from %s' % endpoint
